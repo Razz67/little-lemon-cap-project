e4301c2becd26b5b81f7c41aaa53e307417d650b
Razz67 <srasberry00@gmail.com>
2023-07-29 09:25:29 -0400

    Create Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
 function App() {
-	return <div className="App">Homepage</div>;
+	return (
+	<>
+		<Nav />
+		<Header />
+		<Main />
+		<Footer />
+		
+	</>
+	);
 }
 
 export default App;
